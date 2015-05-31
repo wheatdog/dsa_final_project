7f0cc8829d476a6c89ace24dafbc1d753e9990ad
jackytie <ykcajaaaaa@gmail.com>
2015-05-31 22:29:25 +0800

    modify bank.h(Jacky)

--- code/bank.h
+++ code/bank.h
@@ -4,31 +4,47 @@
 
 using namespace std;
 
-class History {
+class Record {
 	long long int money;
 	time_t time;
-	string fromID;
+}
+class History {
 	string toID;
+	vector<Record> list;
 };
-
 class Account {
+	string ID;
+	string passwd;
 	long long int money;
-	vector<History> history;
+	Tree<History> history;
+	vector<Tree<History>> history;
 
 	public:
-	void withdraw();
-	void deposit();
-	void search();
+	long long int get_money_amount();
+	int withdraw(long long int money);
+	void deposit(long long int money);
+	void search(string ID);
 };
 
-class Bank{
+class Bank {
 	int numAccount;
 	Tree<Account> data;
 
 	public:
-	void creat_account();
-	void delete_account();
-	Account login();
-	void transfer(Account acc, string ID);
-	Account merge(Account acc, string ID);
+	int creat_account();
+	void recommend(bool isExist, string ID, int num);
+	int delete_account();
+	int login(string ID, string passwd, Account* &ptrAccount);
+	int merge(string IDFormer, string passwdFormer, string IDLatter, string passwdLatter);
+	int transfer(Account* ptrFromAccount, string toAccountID, long long int money);
+	void find_wildcard_ID(string wildcardID);
+
 };
+enum result {
+	SUCCESS, FAIL,
+	ID_NOT_FOUNT, WRONG_PASSWD,  //for login, delete
+	ID1_NOT_FOUNT, WRONG_PASSWD1, ID2_NOT_FOUNT, WRONG_PASSWD2,  //for merge
+	ID_EXIST  //for create
+	NO_RECORD  //for search
+};
+
