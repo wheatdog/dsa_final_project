ada0576b4719ff3c4b64fc729e7e7e413f47a30e
jackytie <ykcajaaaaa@gmail.com>
2015-06-03 17:43:10 +0800

    add md5 files and bank2.cpp

--- code/bank.h
+++ code/bank.h
@@ -1,57 +1,67 @@
 #include <vector>
-#include <ctime>
 #include <string>
+#include <map>
+
+#define FROM true
+#define TO false
 
 using namespace std;
 
+typedef long long int Money;
+
 class Record {
-	long long int money;
-	time_t time;
-
-	public:
-	Record(long long int moneyTransfer, time_t timeTransfer);
+    // NOTE: to this account, From => true, TO => false
+    bool type; 
+    Money money; 
+    unsigned long long int time;
 };
-class History {
-	string toID;
-	vector<Record> list;
 
-	public:
-	History(string toID);
-};
+typedef vector<Record> HistoryList;
+
 class Account {
-	string ID;
-	string passwd;
-	long long int money;
-	Tree<History> history;
+    string ID;
+    string passwd;
+    Money money;
+    map<string, HistoryList> history;
 
-	public:
-	Account(string initID, string initPasswd);
-	long long int get_money_amount();
-	int withdraw(long long int money);
-	void deposit(long long int money);
-	int search(string ID);
+    public:
+    Acount(string initID, string initPasswd);
+    Money get_money_amount();
+    int withdraw(Money _money);
+    void deposit(Money _money);
+    int search(string ID);
 };
 
 class Bank {
-	int numAccount;
-	Tree<Account> data;
+    int numAccount;
+    map<string, Account> data;
+    unsigned long long int history_counter;
 
-	public:
-	Bank();
-	int creat_account(string ID, string passwd);
-	void recommend_and_print_ID(bool isExist, string ID, int num);
-	int delete_account(string ID, string passwd);
-	int login(string ID, string passwd, Account* &ptrAccount);
-	int merge(string IDFormer, string passwdFormer, string IDLatter, string passwdLatter);
-	int transfer(Account* ptrFromAccount, string toAccountID, long long int money);
-	void find_and_print_wildcard_ID(string wildcardID);
+    public:
 
+    Bank();
+
+    // TODO: check if ID exist, otherwise create.
+    int create_account(string ID, string password);
+    void recommend_and_print_ID(bool isExist, string ID, int num); 
+    int delete_account(string ID, string password); 
+    int login(string ID, string passwd, Account* &ptrAccount); 
+   
+    // NOTE: maybe a more efficient way?
+    void find_and_print_wildcard_ID(string wildcardID);
+
+    int merge(string IDFormer, string passwdFormer, string IDLatter, string passwdLatter);
+    int transfer(Account* ptrFromAccount, string toAccountID, Money _money);
+
+    friend class Account; 
+    friend class Record;
 };
+
 enum result {
-	SUCCESS, FAIL,
-	ID_NOT_FOUNT, WRONG_PASSWD,  //for login, delete
-	ID1_NOT_FOUNT, WRONG_PASSWD1, ID2_NOT_FOUNT, WRONG_PASSWD2,  //for merge
-	ID_EXIST,  //for create
-	NO_RECORD  //for search
+    SUCCESS, FAIL,
+    ID_NOT_FOUNT, WRONG_PASSWD,  // for login, delete, transfer
+    ID1_NOT_FOUNT, WRONG_PASSWD1, ID2_NOT_FOUNT, WRONG_PASSWD2,  // for merge
+    ID_EXIST,  // for create
+    NO_RECORD  // for search
 };
 
---
+++ code/bank2.cpp
@@ -0,0 +1,35 @@
+#include "md5.h"
+Acount::Acount(string initID, string initPasswd): ID(initID), money(0){
+    passwd = md5(initPasswd);
+}
+int Bank::create_account(string ID, string password){
+    if(data.count(ID) != 0)
+	    return ID_EXIST;
+
+    Account newAccount(ID, password);
+    data[ID] = newAccount;
+    return SUCCESS;
+}
+void Bank::recommend_and_print_ID(bool isExist, string ID, int num);
+int Bank::delete_account(string ID, string password){
+    map<string, Account>::iterator it = data.find(ID);
+    if(it == data.end())
+	    return ID_NOT_FOUNT;
+
+    if(it->passwd != md5(password))
+	    return WRONG_PASSWD;
+
+    data.erase(it);
+    return SUCCESS;
+}
+int Bank::login(string ID, string passwd, Account* &ptrAccount){
+    map<string, Account>::iterator it = data.find(ID);
+    if(it == data.end())
+	    return ID_NOT_FOUNT;
+
+    if(it->passwd != md5(passwd))
+	    return WRONG_PASSWD;
+
+    ptrAccount = &(it->second);
+    return SUCCESS;
+}
